e0db3c90646900009dbe70fda3d4247922b6a56b
Jason Morcos <jason.morcos@moballo.com>
2016-12-16 01:26:21 -0500

    WatchOS Support?

---
+++ MOBCore Watch/MOBCore Watch.h
@@ -0,0 +1,19 @@
+//
+//  MOBCore Watch.h
+//  MOBCore Watch
+//
+//  Created by Jason Morcos on 12/16/16.
+//  Copyright © 2016 tid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MOBCore Watch.
+FOUNDATION_EXPORT double MOBCore_WatchVersionNumber;
+
+//! Project version string for MOBCore Watch.
+FOUNDATION_EXPORT const unsigned char MOBCore_WatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MOBCore_Watch/PublicHeader.h>
+
+
--- MOBCore.podspec
+++ MOBCore.podspec
@@ -9,6 +9,7 @@ Pod::Spec.new do |s|
   s.social_media_url = 'https://twitter.com/_tid_'
 
   s.ios.deployment_target = '8.0'
+  s.watchos.deployment_target = '2.0'
 
   s.framework = 'UIKit'
   s.framework = 'CoreLocation'
